afa2e8baf83bc1c16666ebdfca7897886a20e94a
ShowTakano <mot.innovation.show@gmail.com>
2026-05-11 22:06:43 +0900

    add docker

---
+++ docker/build_docker.sh
@@ -0,0 +1,1 @@
+docker build -t pytorch26_rl -f docker/Dockerfile .
---
+++ docker/run_docker.sh
@@ -0,0 +1,4 @@
+docker run -it --rm \
+--gpus all \
+-v //e/git/deep-learning-from-scratch-4:/deep-learning-from-scratch-4 \
+pytorch26_rl
